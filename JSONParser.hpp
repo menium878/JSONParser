53ae5305f9a6392a563d1b64cb217cdf7c1df88e
Menium <meus13@gmail.com>
2025-06-28 23:33:18 +0200

    Start of lexer

---
+++ JSONParser.hpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <string_view>
+#include <variant>
+enum class TokenType {
+    LEFT_BRACE,    // {
+    RIGHT_BRACE,   // }
+    LEFT_BRACKET,  // [
+    RIGHT_BRACKET, // ]
+    COLON,         // :
+    COMMA,         // ,
+    STRING,
+    NUMBER,
+    TRUE, FALSE, NULL_VALUE,
+    END_OF_INPUT,
+    ERROR
+};
+using TokenValue = std::variant<std::monostate, double, std::string, bool, std::nullptr_t>;
+struct Token {
+    TokenType type;
+    TokenValue text;
+};
+
+class Lexer{
+    public:
+        Lexer(std::string_view input) : input_(input), pos_(0) {}
+        void next_token(Token &) noexcept;
+    private:
+        std::string_view input_;
+        size_t pos_;
+
+        char peek() const;
+        char advance();
+    void skip_whitespace();
+    Token string();
+    Token number();
+    Token literal_error();
+};
